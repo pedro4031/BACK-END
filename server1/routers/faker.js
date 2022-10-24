528b5ae45be34ecdf7d6defb176069dcbf76a050
pedro4031 <pedrocasabonat@gmail.com>
2022-10-23 23:31:29 -0300

    desafio server con balance de cargas

---
+++ server1/routers/faker.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const { faker } = require('@faker-js/faker');
+const { checkAuthentication } = require('./funciones');
+const routerFaker = express.Router();
+
+routerFaker.get('/api/productos-test', checkAuthentication, (req, res) => {
+  let productosRandom = [];
+
+  for (let i = 0; i < 5; i++) {
+    let prod = {};
+    const randomName = faker.commerce.product();
+    const randomPrice = faker.commerce.price(50, 10000);
+    const randomFoto = faker.image.food(150, 100, true);
+    prod.name = randomName;
+    prod.price = randomPrice;
+    prod.foto = randomFoto;
+    productosRandom.push(prod);
+  }
+  res.json(productosRandom);
+});
+
+module.exports = routerFaker;
